d62e249d647f3937ff99dea5245d3f6c12e5c175
vvallker <96576891+grooptroop@users.noreply.github.com>
2024-12-03 00:08:50 +0300

    srok11

--- s_project/s/urls.py
+++ s_project/s/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from .views import schedule_calendar, table_view, krasota, blackjack, kaz
 
 urlpatterns = [
-path('', blackjack, name='krasota'),
+path('', kaz, name='krasota'),
 path('f', schedule_calendar, name='shedule'),
 path('LAB', table_view, name='table'),
-path('Fuf', kaz, name='Fuf'),
-path('kaz', kaz, name='kaz'),
+path('Fuf', krasota, name='Fuf'),
+path('kaz', blackjack, name='kaz'),
 
 ]
